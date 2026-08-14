529ac408d79ff032603c9b3b89704b753f712a33
Gunnar Arctaedius <gunnar@arctaedius.com>
2026-08-15 00:15:40 +0200

    added data and very simple load

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 import pandas as pd
+
+hot_100 = pd.read_csv("data\\hot_100.csv", parse_dates=["chart_date"])
+# only keep 90s
+hot_100 = hot_100[("1990-01-01" < hot_100.chart_date) & (hot_100.chart_date< "2000-01-01")].sort_values("chart_date")
