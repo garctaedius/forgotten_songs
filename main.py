64ddf4782068b7491d655245d9be7c30a878f173
Gunnar Arctaedius <gunnar@arctaedius.com>
2026-08-15 23:39:55 +0200

    Added the start to a bonus system

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from math import floor, ceil
 
 # Gather hot100 data
 hot100 = pd.read_csv("data\\hot_100.csv", parse_dates=["chart_date", "chart_debut"])
@@ -6,6 +7,19 @@ nineties = hot100[("1990-01-01" < hot100.chart_date) & (hot100.chart_date< "2000
 
 def score(chart_position: int) -> float:
     return -4357*(chart_position/(chart_position+10)) + 4139
+
+def triangle_numbers(k: int) -> float:
+    return (k*(k+1))/2
+
+def bonus(n: int) -> float:
+    bonus_value = 8.8  # TODO: figure out the values for #2-#10
+    if n==2:
+        return 0.5 * bonus_value
+
+    factor = triangle_numbers(ceil(n/2)) + triangle_numbers(floor(n/2))
+
+    return factor * bonus_value
+    
 
 # Calculate score for each song in the nineties
 song_id = 'SmoothSantana Featuring Rob Thomas'
