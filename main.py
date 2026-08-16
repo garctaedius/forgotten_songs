029cb34b028bfbce04866017600a399987357604
Gunnar Arctaedius <gunnar@arctaedius.com>
2026-08-16 10:18:38 +0200

    scaled results and added bonus for ranks 1-10

--- main.py
+++ main.py
@@ -6,26 +6,38 @@ hot100 = pd.read_csv("data\\hot_100.csv", parse_dates=["chart_date", "chart_debu
 hot100["previous_week"] = hot100.previous_week.fillna(0)  # simpler than having na values
 nineties = hot100[("1990-01-01" < hot100.chart_date) & (hot100.chart_date< "2000-01-01")].sort_values("chart_date")
 
-def score(chart_position: int) -> float:
+def raw_score(chart_position: int) -> float:
     return -4357*(chart_position/(chart_position+10)) + 4139
 
-def triangle_numbers(k: int) -> float:
-    return (k*(k+1))/2
+scale_factor = 100/raw_score(1)
+def score(chart_position: int) -> float:
+    return raw_score(chart_position)*scale_factor
 
-def bonus(n: int) -> float:
-    bonus_value = 8.8  # TODO: figure out the values for #2-#10
-    if n==2:
-        return 0.5 * bonus_value
 
-    factor = triangle_numbers(ceil(n/2)) + triangle_numbers(floor(n/2))
+# TODO: add variable for how man weeks get bonuses
+scores = {i: score(i) for i in range(1,12)}
+bonus_value = {
+    i: scores[i]-scores[i+1]
+    for i in range(1,11)
+}
 
-    return factor * bonus_value
+def mod_triangle_numbers(k: int) -> float:
+    return (k*(k-1))/2
+
+def bonus(n_weeks: int, rank: int) -> float:
+    if n_weeks==2:
+        return 0.5 * bonus_value[rank]
+
+    factor = mod_triangle_numbers(ceil(n_weeks/2)) + mod_triangle_numbers(floor(n_weeks/2))
+
+    return factor * bonus_value[rank]
     
 
 # Calculate score for each song in the nineties
 song_id = 'SmoothSantana Featuring Rob Thomas'
 occurances = hot100[hot100.song_id==song_id].copy()
 occurances["score"] = occurances.chart_position.apply(score)
+full_chart_score = occurances.score.sum()
 
 streaks = []
 current_streak = None
@@ -42,7 +54,8 @@ for chart in occurances[["chart_position", "previous_week"]].itertuples():
     else:
         current_streak = (chart.chart_position, current_streak[1]+1)
 
-occurances["bonus"] = 0
+song_bonus = sum([bonus(streak[1], streak[0]) for streak in streaks])
+total_score = full_chart_score + song_bonus
 
 
 pass
