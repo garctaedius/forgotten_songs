f2d48c09dc97d85e0aeac56bb38410104e465393
Gunnar Arctaedius <gunnar@arctaedius.com>
2026-08-18 10:33:15 +0200

    format

--- tests/test_random_song.py
+++ tests/test_random_song.py
@@ -1,15 +1,16 @@
+from spotapi import Song
 import pandas as pd
 
 hot100 = pd.read_csv("data\\hot_100.csv", parse_dates=["chart_date", "chart_debut"])
 # only keep 90s
-hot100 = hot100[("1990-01-01" < hot100.chart_date) & (hot100.chart_date< "2000-01-01")].sort_values("chart_date")
+hot100 = hot100[("1990-01-01" < hot100.chart_date) & (hot100.chart_date < "2000-01-01")].sort_values("chart_date")
 
-from spotapi import Song
 
 song = Song()
 
+
 def search_song(artist, title, year, limit=1):
-    query =  f'track:"{title}" artist:"{artist}" year:1985-{year}'
+    query = f'track:"{title}" artist:"{artist}" year:1985-{year}'
 
     results = song.query_songs(query, limit=limit)
 
@@ -21,6 +22,7 @@ def search_song(artist, title, year, limit=1):
     plays = int(song.get_track_info(track_id=top_track["id"])["data"]["trackUnion"]["playcount"])
 
     return track_name, first_artist_name, plays
+
 
 def test_random_song():
     random_song = hot100.sample().iloc[0]
